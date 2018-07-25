741233cdd8e624de282ae8a7550561d26ffadc8b
Dennis Eisold <de@itstall.de>
2018-07-25 15:49:59 +0200

    added ram usage to system_status

--- system_status.py
+++ system_status.py
@@ -1,4 +1,6 @@
-import os, json, time
+from __future__ import division
+from subprocess import PIPE, Popen
+import os, json, time, psutil
 import paho.mqtt.client as mqtt
 
 try:
@@ -66,20 +68,21 @@ def getDiskSpace():
             return(line.split()[1:5])
 
 while True:
-	data["CPU_temp"] = getCPUtemperature()
-	data["CPU_usage"] = getCPUuse()
-	data["RAM_stats"] = getRAMinfo()
-	data["RAM_total"] = round(int(data["RAM_stats"][0]) / 1000,1)
-	data["RAM_used"] = round(int(data["RAM_stats"][1]) / 1000,1)
-	data["RAM_free"] = round(int(data["RAM_stats"][2]) / 1000,1)
+    ram = psutil.virtual_memory()
+    data["CPU_temp"] = getCPUtemperature()
+    data["CPU_usage"] = getCPUuse()
+    data["RAM_total"] = ram.total / 2**20
+    data["RAM_used"] = ram.used / 2**20
+    data["RAM_free"] = ram.free / 2**20
+    data["RAM_usage"] = ram.percent
     if(config[config_set]["fan_topic"]):
         if(data["CPU_temp"] > 60.0):
-            client.publish(config[config_set]['fan_topic'] + 2, 100)
+            client.publish(config[config_set]['fan_topic'] + "2", "100")
         if(data["CPU_temp"] < 40.0):
-            client.publish(config[config_set]['fan_topic'] + 2, 0)
-	
-	mqtt_data = json.dumps(data)
-	client.publish(config[config_set]['mqtt_topic'], mqtt_data)
-	mqtt_main.publish(config[config_set]['mqtt_main_topic'], mqtt_data)
-	print(data["CPU_temp"])
-	time.sleep(10)
+            client.publish(config[config_set]['fan_topic'] + "2", "0")
+
+    mqtt_data = json.dumps(data)
+    client.publish(config[config_set]['mqtt_topic'], mqtt_data)
+    mqtt_main.publish(config[config_set]['mqtt_main_topic'], mqtt_data)
+    print(data["CPU_temp"])
+    time.sleep(10)
